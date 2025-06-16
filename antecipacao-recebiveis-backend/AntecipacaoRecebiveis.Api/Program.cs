f59c6327ef11909b79520b1f35c1d14ebb305d66
luizolivas <luizolivas@gmail.com>
2025-06-16 00:02:30 -0300

    chore: configura banco de dados inMemory

--- antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Api/Program.cs
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Api/Program.cs
@@ -1,3 +1,11 @@
+using AntecipacaoRecebiveis.Application.Interfaces;
+using AntecipacaoRecebiveis.Application.Services;
+using AntecipacaoRecebiveis.Infrastructure.Data;
+using AntecipacaoRecebiveis.Infrastructure.Interfaces;
+using AntecipacaoRecebiveis.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("AntecipacaoDb"));
+
+builder.Services.AddScoped<INfeService, NfeService>();
+builder.Services.AddScoped<INfeRepository, NfeRepository>();
 
 var app = builder.Build();
 
