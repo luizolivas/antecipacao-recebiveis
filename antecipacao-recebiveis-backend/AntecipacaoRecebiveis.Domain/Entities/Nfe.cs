9e06444fe71fa878ce0b95faa981ec1ad2ce870d
luizolivas <luizolivas@gmail.com>
2025-06-16 01:39:45 -0300

    fix: corrige atributo valor da nfe

--- antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Domain/Entities/Nfe.cs
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Domain/Entities/Nfe.cs
@@ -5,6 +5,7 @@ using AntecipacaoRecebiveis.Domain.Entities;
     {
         public int Id { get; set; }
         public string Number { get; set; }
+        public decimal Value { get; set; }
         public DateTime ExpirationDate { get; set; }
         public int CompanyId { get; set; }
         public Company Company { get; set; }
