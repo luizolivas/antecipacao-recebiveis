43db9a7aaf6a8fe9d22d956d1232526c11136365
luizolivas <luizolivas@gmail.com>
2025-06-16 18:52:29 -0300

    feat: cria DTOs para Company e Nfe

---
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Application/DTOs/CompanyDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntecipacaoRecebiveis.Application.DTOs
+{
+    public class CompanyDto
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string cnpj { get; set; }
+        public decimal faturamentoMensal { get; set; }
+        public Sector setor { get; set; }
+        public decimal creditLimit { get; set; }
+    }
+}
---
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Application/DTOs/NfeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntecipacaoRecebiveis.Application.DTOs
+{
+    public class NfeDto
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int CompanyId { get; set; } 
+    }
+}
