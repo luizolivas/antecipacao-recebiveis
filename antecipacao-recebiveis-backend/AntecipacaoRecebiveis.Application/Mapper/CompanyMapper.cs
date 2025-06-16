9055794dc1cc6de384689c14007611236fd7b129
luizolivas <luizolivas@gmail.com>
2025-06-16 18:52:49 -0300

    feat: adiciona mapeamentos de entidade para DTO e vice-versa

---
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Application/Mapper/CompanyMapper.cs
@@ -0,0 +1,37 @@
+﻿using AntecipacaoRecebiveis.Application.DTOs;
+using AntecipacaoRecebiveis.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntecipacaoRecebiveis.Application.Mapper
+{
+    public static class CompanyMapper
+    {
+        public static CompanyDto ToDto(this Company company)
+        {
+            return new CompanyDto {
+                Id = company.Id,
+                nome = company.Name,
+                cnpj = company.Cnpj, // exemplo
+                faturamentoMensal = company.MonthlyBiling,
+                setor = company.Sector,
+                creditLimit = company.CreditLimit
+            };
+        }
+
+        public static Company ToEntity(this CompanyDto dto)
+        {
+            return new Company {
+                Id = dto.Id,
+                Name = dto.nome,
+                MonthlyBiling = dto.faturamentoMensal,
+                Sector = dto.setor,
+                CreditLimit = dto.creditLimit,
+                Cnpj = dto.cnpj               
+            };
+        }
+    }
+}
---
+++ antecipacao-recebiveis-backend/AntecipacaoRecebiveis.Application/Mapper/NfeMapper.cs
@@ -0,0 +1,34 @@
+﻿using AntecipacaoRecebiveis.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntecipacaoRecebiveis.Application.Mapper
+{
+    public static class NfeMapper
+    {
+        public static NfeDto ToDto(this Nfe nfe)
+        {
+            return new NfeDto {
+                Id = nfe.Id,
+                Numero = nfe.Number,
+                Valor = nfe.Value,
+                DataVencimento = nfe.ExpirationDate,
+                CompanyId = nfe.CompanyId
+            };
+        }
+
+        public static Nfe ToEntity(this NfeDto dto)
+        {
+            return new Nfe {
+                Id = dto.Id,
+                Number = dto.Numero,
+                Value = dto.Valor,
+                ExpirationDate = dto.DataVencimento,
+                CompanyId = dto.CompanyId
+            };
+        }
+    }
+}
